ff5869017e835cbf553d79f8227414ddd2dc5d8b
Ciprian Badescu <cbadescu@elvis.mrc.alcatel.ro>
2011-12-09 03:59:26 -0500

    not sure

--- bin/kmount.sh
+++ bin/kmount.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# add following line to fstab
+#/dev/mapper/kingstone1G /mnt/kingstone1G    ext3 user,noauto     0       0
+
 sudo cryptsetup luksOpen /dev/sdb1 kingstone1G 
-sudo mount /dev/mapper/kingstone1G /mnt/kingstone1G
+mount /mnt/kingstone1G
+cd /mnt/kingstone1G
