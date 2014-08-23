8f52f5f47931edb0d4f12f628b22ecc42cdde38c
VKI <lib.aca55a@gmail.com>
2014-08-23 19:14:07 +0300

    tune haskell mode

--- apps/emacs/emacs-packages.el
+++ apps/emacs/emacs-packages.el
@@ -77,17 +77,17 @@
 ;; EL-GET
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+(with-current-buffer
+    (url-retrieve-synchronously
+     "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+  (goto-char (point-max))
+  (eval-print-last-sexp)))
 (defun el-get-sync-recipes (overlay)
-  (let* ((recipe-glob (locate-user-emacs-file (concat overlay "/recipes/*.rcp")))
-         (recipe-files (file-expand-wildcards recipe-glob))
-         (recipes (mapcar 'el-get-read-recipe-file recipe-files)))
-    (mapcar (lambda (r) (add-to-list 'el-get-sources r)) recipes)
-    (el-get 'sync (mapcar 'el-get-source-name recipes))))
+(let* ((recipe-glob (locate-user-emacs-file (concat overlay "/recipes/*.rcp")))
+       (recipe-files (file-expand-wildcards recipe-glob))
+       (recipes (mapcar 'el-get-read-recipe-file recipe-files)))
+  (mapcar (lambda (r) (add-to-list 'el-get-sources r)) recipes)
+  (el-get 'sync (mapcar 'el-get-source-name recipes))))
 (setq el-get-user-package-directory user-emacs-directory)
 ;; EL-GET SYNC OVERLAYS
 (el-get-sync-recipes "el-get-haskell")
@@ -835,11 +835,13 @@ Key bindings:
 			    ))
 
 
-;; Haskell
+;; ;; Haskell
 (require 'haskell-mode)
 (require 'inf-haskell)
 (add-hook 'haskell-mode-hook 'haskell-indent-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(require 'ghci-completion)
+(add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion)
 (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion)
 
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-hugs)
@@ -851,6 +853,10 @@ Key bindings:
 (custom-set-variables
  '(haskell-program-name "ghci"))
 ;; (setq haskell-font-lock-symbols t)
+(add-hook 'haskell-mode-hook '(lambda ()
+                                (local-set-key "\C-c\C-d" 'haskell-hoogle)
+
+                                ))
 
 
 ;; Yasnippet
